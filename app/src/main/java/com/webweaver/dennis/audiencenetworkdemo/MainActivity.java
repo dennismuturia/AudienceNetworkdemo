c410a3dd1145fc1847c170a633cebbe502490b89
dennismuturia <dmuturia2922@gmail.com>
2018-05-02 10:58:44 +0300

    Initial commit

---
+++ app/src/main/java/com/webweaver/dennis/audiencenetworkdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.webweaver.dennis.audiencenetworkdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
