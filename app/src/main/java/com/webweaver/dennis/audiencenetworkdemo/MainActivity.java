d02cb2b6c2d53791e1a3323d1196310fe7a72d11
dennismuturia <dmuturia2922@gmail.com>
2018-05-03 12:29:51 +0300

    Added the hardware acceleration to support video ads.

--- app/src/main/java/com/webweaver/dennis/audiencenetworkdemo/MainActivity.java
+++ app/src/main/java/com/webweaver/dennis/audiencenetworkdemo/MainActivity.java
@@ -28,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showNativeAd();
     }
 
     public void showNativeAd(){
